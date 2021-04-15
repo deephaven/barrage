ba715e5e738788ab03dd3cbb2fcf7778a58f7e66
Mike Bender <mikebender@deephaven.io>
2021-04-15 14:32:00 -0400

    Update package.json, add README.md (#2)
    - Set publishConfig to publish to GitHub packages (we can change to npmjs later, we just don't have an org there yet)
    - Remove postbootstrap script, it wasn't used
    - Renamed lib:build => build, added generating schemas to the build step
    - Added README.md with instructions on how to build a specific version of flatc

--- javascript/generateSchemas.sh
+++ javascript/generateSchemas.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
+# Generate the flatbuffer Schemas used by Deephaven
 
 set -e
 
 # Must point to a build of flatc that is from sept 2020 or newer
+# In particular, must have this fix: https://github.com/google/flatbuffers/commit/c30a87de6fc741d7dfcf1f721a69d89ca27a0866
 if [ -z "$FLATC" ]; then
   echo "FLATC was not set, attempting to use it from path";
   FLATC='flatc'
