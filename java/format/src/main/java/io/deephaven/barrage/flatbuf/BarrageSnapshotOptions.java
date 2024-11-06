2f884a569b01b1d2614df7c0755b0500ac0341f2
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2024-11-06 16:45:09 -0700

    Make header a year range

--- java/format/src/main/java/io/deephaven/barrage/flatbuf/BarrageSnapshotOptions.java
+++ java/format/src/main/java/io/deephaven/barrage/flatbuf/BarrageSnapshotOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Deephaven Data Labs
+ * Copyright 2020-2024 Deephaven Data Labs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
