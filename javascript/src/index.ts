26bcd85edc5b399d30dedc9c7dca5a9ede85724a
Colin Alworth <colinalworth@deephaven.io>
2025-12-19 09:46:28 -0600

    Remove JS build wiring, users can build from the .fbs file if desired (#52)

--- javascript/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Barrage_generated from '../generated/Barrage_generated';
-
-export namespace io.deephaven.barrage {
-    export const flatbuf = {
-        ...Barrage_generated.io.deephaven.barrage.flatbuf
-    };
-}
